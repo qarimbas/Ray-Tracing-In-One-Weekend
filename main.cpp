9c5e209455a7c7f11ed0c653dfe0e7275879a4df
s21kkurt <kkurt@sabanciuniv.edu>
2021-10-20 21:24:12 +0300

    vec3 h

--- main.cpp
+++ main.cpp
@@ -11,8 +11,9 @@ int main() {
 
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-	for (int j = image_height - 1; j >= 0; j--) {
-		for (int i = 0; i < image_width; i++) {
+	for (int j = image_height - 1; j >= 0; --j) {
+		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+		for (int i = 0; i < image_width; ++i) {
 			auto r = double(i) / (image_width - 1);
 			auto g = double(j) / (image_height - 1);
 			auto b = 0.25;
@@ -24,4 +25,5 @@ int main() {
 			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
 		}
 	}
+	std::cerr << "\nDone.\n";
 }
